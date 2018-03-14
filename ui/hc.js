9dfee6f9c9b0166a4442d8417de18a88adab6c03
rasika <rasika.chafekar@gmail.com>
2018-03-14 02:38:29 -0700

    Revert "consume holodex ready with UI integrated"
    This reverts commit 3cdf9df237923d5cffcabf9fdae8028d4238ceec.

--- ui/hc.js
+++ ui/hc.js
@@ -1,14 +1,1 @@
-function send(fn,data,resultFn) {
-    console.log("calling: " + fn+" with "+data);
-    $.post(
-        "/fn/consumeHolodex/"+fn,
-        data,
-        function(response) {
-            console.log("response: " + response);
-            resultFn(response);
-        }
-    ).error(function(response) {
-        console.log("response failed: " + response.responseText);
-    })
-    ;
-};
+function yourApp(){alert('your UI code here!')}
