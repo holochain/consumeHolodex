eff8d7dcdcb96e7e8a9a4e79d50b12daae3cbf7b
rasika <rasika.chafekar@gmail.com>
2018-03-14 03:04:08 -0700

    consumeHolodex UI added all tests running

--- ui/hc.js
+++ ui/hc.js
@@ -1,1 +1,14 @@
-function yourApp(){alert('your UI code here!')}
+function send(fn,data,resultFn) {
+    console.log("calling: " + fn+" with "+data);
+    $.post(
+        "/fn/consumeHolodex/"+fn,
+        data,
+        function(response) {
+            console.log("response: " + response);
+            resultFn(response);
+        }
+    ).error(function(response) {
+        console.log("response failed: " + response.responseText);
+    })
+    ;
+};
